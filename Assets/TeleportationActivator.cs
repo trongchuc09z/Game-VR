5390adcb827c42dd5740524a333b5ea812eef15c
TrongChuc <nguyentrongchuc2k4@gmail.com>
2026-03-22 16:12:22 +0700

    Feat : Hand Tele, move

---
+++ Assets/TeleportationActivator.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
+
+public class TeleportationActivator : MonoBehaviour
+{
+    public InputActionProperty teleportActivatorAction;
+    public XRRayInteractor teleportInteractor;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        teleportInteractor.gameObject.SetActive(false);
+        teleportActivatorAction.action.performed += Action_performed;
+    }
+
+    private void Action_performed(InputAction.CallbackContext obj)
+    {
+        teleportInteractor.gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(teleportActivatorAction.action.WasReleasedThisFrame())
+        {
+            teleportInteractor.gameObject.SetActive(false);
+        }
+    }
+}
