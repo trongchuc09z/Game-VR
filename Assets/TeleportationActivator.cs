74d6234769af127e39fd3c83ff740726f5dc6331
TrongChuc <nguyentrongchuc2k4@gmail.com>
2026-03-23 11:38:10 +0700

    Feat : poke & UI

--- Assets/TeleportationActivator.cs
+++ Assets/TeleportationActivator.cs
@@ -5,17 +5,29 @@ using UnityEngine.XR.Interaction.Toolkit.Interactors;
 public class TeleportationActivator : MonoBehaviour
 {
     public InputActionProperty teleportActivatorAction;
+    public XRRayInteractor rayInteractor;
     public XRRayInteractor teleportInteractor;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         teleportInteractor.gameObject.SetActive(false);
         teleportActivatorAction.action.performed += Action_performed;
+        rayInteractor.uiHoverEntered.AddListener(x => DisaleTeleporRay());
     }
 
     private void Action_performed(InputAction.CallbackContext obj)
     {
+        if(rayInteractor && rayInteractor.IsOverUIGameObject())
+        {
+            return;
+        }
+
         teleportInteractor.gameObject.SetActive(true);
+    }
+
+    public void DisaleTeleporRay()
+    {
+        teleportInteractor.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
