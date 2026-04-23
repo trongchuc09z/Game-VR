7c3048ed9bc6057e862de5198f4d9c190fda1439
TrongChuc <nguyentrongchuc2k4@gmail.com>
2026-04-24 00:21:50 +0700

    Update : Opm

--- Assets/Scripts/FadeManager.cs
+++ Assets/Scripts/FadeManager.cs
@@ -1,4 +1,4 @@
-using TMPro;
+﻿using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,6 +9,10 @@ public class FadeManager : MonoBehaviour
 
     void Start()
     {
+        QualitySettings.vSyncCount = 0;
+
+        // Khóa FPS ở mức 60 (bạn có thể thay đổi số này thành 30, 90, 120...)
+        Application.targetFrameRate = 72;
         fade.gameObject.SetActive(true);
         fade.CrossFadeAlpha(0f, fadeDuration, false);
     }
